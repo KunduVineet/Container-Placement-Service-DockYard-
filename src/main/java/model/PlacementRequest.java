ed6011197385f08f3ca2ab8dd78a3450658ae3d0
VineetKundu <kunduvineet6@gmail.com>
2025-04-24 11:08:27 +0530

    Models Created- slot, Container and more

--- src/main/java/model/PlacementRequest.java
+++ src/main/java/model/PlacementRequest.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.util.List;
+
 public class PlacementRequest {
+
+    private Container container;
+    private List<Slot> yardMap;
+
+    public Container getContainer() {
+        return container;
+    }
+
+    public void setContainer(Container container) {
+        this.container = container;
+    }
+
+    public List<Slot> getYardMap() {
+        return yardMap;
+    }
+
+    public void setYardMap(List<Slot> yardMap) {
+        this.yardMap = yardMap;
+    }
 }
--- src/main/java/model/PlacementResponse.java
+++ src/main/java/model/PlacementResponse.java
@@ -1,4 +1,50 @@
 package model;
 
 public class PlacementResponse {
+    private String containerId;
+    private Integer targetX;
+    private Integer targetY;
+    private String error;
+
+    public PlacementResponse(String containerId, Integer targetX, Integer targetY) {
+        this.containerId = containerId;
+        this.targetX = targetX;
+        this.targetY = targetY;
+    }
+
+    public PlacementResponse(String error) {
+        this.error = error;
+    }
+
+    public String getContainerId() {
+        return containerId;
+    }
+
+    public void setContainerId(String containerId) {
+        this.containerId = containerId;
+    }
+
+    public Integer getTargetX() {
+        return targetX;
+    }
+
+    public void setTargetX(Integer targetX) {
+        this.targetX = targetX;
+    }
+
+    public Integer getTargetY() {
+        return targetY;
+    }
+
+    public void setTargetY(Integer targetY) {
+        this.targetY = targetY;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
